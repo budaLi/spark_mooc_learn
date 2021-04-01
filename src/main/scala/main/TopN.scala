d81787076d254341584cb3ed3f1b2fb59fac0cac
lenovo <1364826576@qq.com>
2021-04-01 14:12:51 +0800

    按流量统计topN

--- src/main/scala/main/TopN.scala
+++ src/main/scala/main/TopN.scala
@@ -15,43 +15,45 @@ object TopN {
   def main(args: Array[String]): Unit = {
       //通过config 参数可以设置不启用 spark自带的类型判断功能
       val sc = SparkSession.builder().master("local[2]").appName("topN").
-          config("spark.sql.sources.partitionColumnTypeInference.enabled", false).getOrCreate()
-      val df = sc.read.format("parquet").load("E:\\桌面\\工作算法\\spark_mooc_learn\\src\\main\\resources\\clear")
-      df.printSchema()
-      df.show(false)
+        config("spark.sql.sources.partitionColumnTypeInference.enabled", false).getOrCreate()
+    val df = sc.read.format("parquet").load("E:\\桌面\\工作算法\\spark_mooc_learn\\src\\main\\resources\\clear")
+    df.printSchema()
+    df.show(false)
 
-      val day = "2017-05-11"
-      val courseType = "article"
+    val day = "2017-05-11"
+    val courseType = "article"
 
 
-//    count_topN_by_dataframe(sc,df,day,courseType)
-    count_topN_by_sql(sc,df,day,courseType)
+    //    count_topN_times_by_dataframe(sc,df,day,courseType)
+    //      count_topN_times_by_sql(sc,df,day,courseType)
+    count_topN_traffic_by_dataframe(sc, df, day, courseType)
+    //      count_topN_traffic_by_sql(sc,df,day,courseType)
 
   }
 
   // 统计某一天每个课程的访问次数
-  def count_topN_by_dataframe(spark:SparkSession,df:DataFrame,day:String,courseType:String): Unit ={
+  def count_topN_times_by_dataframe(spark: SparkSession, df: DataFrame, day: String, courseType: String): Unit = {
 
     //导入隐式声明
     import spark.implicits._
     // 使用dataframe方法统计
-    val dayTopDF = df.filter($"day"===day && $"courseType"===courseType)
+    val dayTopDF = df.filter($"day" === day && $"courseType" === courseType)
       // 按天和课程id分组
-      .groupBy("day","courseId")
-//      按courseId聚合 并降序排列
+      .groupBy("day", "courseId")
+      //      按courseId聚合 并降序排列
       .agg(count("courseId").as("times")).orderBy("times")
     dayTopDF.show(false)
   }
 
-  def count_topN_by_sql(spark: SparkSession,df: DataFrame,day:String,courseType:String): Unit ={
+  def count_topN_times_by_sql(spark: SparkSession, df: DataFrame, day: String, courseType: String): Unit = {
 
     //创建一个临时试图表
     // 注意字符串的格式化及sql语句的空格
     df.createOrReplaceTempView("clean_log")
-    val sqlText = "select day,courseId,count(1) as times from clean_log "+
-                  s"where day='$day' and courseType='$courseType' "+
-                  "group by day,courseId "+
-                  "order by times desc"
+    val sqlText = "select day,courseId,count(1) as times from clean_log " +
+      s"where day='$day' and courseType='$courseType' " +
+      "group by day,courseId " +
+      "order by times desc"
 
     val sqlText2 = "select day,courseId,count(1) as times " +
       "from clean_log " +
@@ -64,7 +66,7 @@ object TopN {
     val dayTopDF = spark.sql(sqlText)
     dayTopDF.show(false)
 
-    //TODO 将topN的数据插入数据库
+    // 将topN的数据插入数据库
     try {
       dayTopDF.foreachPartition(partition=>{
         //准备dayTop类型的listBuffer
@@ -82,13 +84,40 @@ object TopN {
 
         //通过封装好的工具包存入数据库
         Dao.insertDayTop(list)
-        }
+      }
       )
     }
     catch {
-      case e:Exception=>e.printStackTrace()
+      case e: Exception => e.printStackTrace()
     }
 
+  }
 
+  //统计某天某种类型课程的流量topN 课程ID dataframe方式
+  def count_topN_traffic_by_dataframe(spark: SparkSession, df: DataFrame, day: String, courseType: String): Unit = {
+    //按dataframe统计 按day和courseType 过滤出数据 在通过courseId分组  将流量聚合 降序排列
+    //聚合后的as重命名括号位置要注意
+    import spark.implicits._
+    val topN_traffic = df.filter($"day" === day && $"courseType" === courseType).
+      groupBy("day", "courseId").agg(sum("traffic").as("totle_traffic")).orderBy("totle_traffic")
+
+    topN_traffic.show(10)
+
+    //入库处理
+  }
+
+
+  //统计某天某种类型课程的流量topN 课程ID
+  def count_topN_traffic_by_sql(spark: SparkSession, df: DataFrame, day: String, courseType: String): Unit = {
+    //sql查询方式  建立临时表视图 定义sql spark 提交sql
+    df.createOrReplaceTempView("topN_traffic")
+
+    //写sql的时候要注意 1.from 表名  2.group by时不用加括号  3. 各单词间有空格
+    val sql_text = "select day,courseId,sum(traffic) as totle_traffic from topN_traffic " +
+      s"where day='$day' and courseType='$courseType' " +
+      "group by day,courseId  " +
+      "order by totle_traffic desc"
+    val topN_traffic = spark.sql(sql_text)
+    topN_traffic.show(10)
   }
 }
